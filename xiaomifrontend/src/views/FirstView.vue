072be4f19265d27194e49f6fcc2822fe616053c2
Sander Chen <sanderxavalon@gmail.com>
2022-01-26 20:49:52 +0800

    新增測試頁面

---
+++ xiaomifrontend/src/views/FirstView.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>Frist View</p>
+</template>
+
+<script>
+    export default {
+        name : 'FirstView',
+        mounted(){
+            this.$axios.get('/api/test')
+            .then(response => {
+                console.log(response);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
