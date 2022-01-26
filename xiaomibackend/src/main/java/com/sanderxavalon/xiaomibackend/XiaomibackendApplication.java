b88834be753ac13ad4fe7085a6496e0df930fb55
Sander Chen <sanderxavalon@gmail.com>
2022-01-26 20:41:18 +0800

    後端專案初始化

---
+++ xiaomibackend/src/main/java/com/sanderxavalon/xiaomibackend/XiaomibackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanderxavalon.xiaomibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomibackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomibackendApplication.class, args);
+    }
+
+}
---
+++ xiaomibackend/src/test/java/com/sanderxavalon/xiaomibackend/XiaomibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanderxavalon.xiaomibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomibackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
