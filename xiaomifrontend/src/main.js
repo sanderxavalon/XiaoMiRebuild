8da3620539079603fd9e5cc5b0dfafffd5843c82
Sander Chen <sanderxavalon@gmail.com>
2022-01-26 20:51:06 +0800

    main.js router中加入攔截requireAuth功能

--- xiaomifrontend/src/main.js
+++ xiaomifrontend/src/main.js
@@ -3,10 +3,26 @@ import './plugins/axios'
 import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
+
+router.beforeEach((to, from , next) => {
+  if (to.meta.requireAuth) {
+    if(store.state.token) {
+      next();
+    } else {
+      next({
+        path: 'login'
+      })
+    }
+  } else {
+    next();
+  }
+})
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
